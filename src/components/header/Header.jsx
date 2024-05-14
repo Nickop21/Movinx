76525fc1af3bdb9ce29e9c2486b3869788d32e9d
Nickop21 <ny8244@gmail.com>
2024-05-15 00:32:03 +0530

    logo and favicon changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/movix-logo.png";
 
 const Header = () => {
     const [show, setShow] = useState("top");
@@ -74,7 +74,7 @@ const Header = () => {
         <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}>
-                    <img src={logo} alt="" />
+                    <img src={logo} alt="" /> <span className="logo-name">Talkix</span>
                 </div>
                 <ul className="menuItems">
                     <li
